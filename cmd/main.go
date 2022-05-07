8a48b00b77c4acc121b5ddd3ed906c7bcaedf203
NateDogg <nathan@necustomsoftware.com>
2022-05-07 18:42:16 -0400

    looping through pins

--- cmd/main.go
+++ cmd/main.go
@@ -18,21 +18,24 @@ func main () {
 	}
 	defer rpio.Close()
 
-	pin := rpio.Pin(10)
+	for p := 0; p < 20; p++ {
+		fmt.Println ("pin ", p)
+		pin := rpio.Pin(p)
 
-	pin.Output()
+		pin.Output()
 
-	for x := 0; x < 300; x++ {
-		fmt.Println ("making high")
-		pin.High() // make it high
-		time.Sleep(time.Second * 2)
-		fmt.Println ("making low")
-		pin.Low()
-		time.Sleep(time.Second * 2)
+		for x := 0; x < 5; x++ {
+			fmt.Println ("making high")
+			pin.High() // make it high
+			time.Sleep(time.Second * 4)
+			fmt.Println ("making low")
+			pin.Low()
+			time.Sleep(time.Second * 2)
+		}
+
+		fmt.Println ("done with pin")
+		pin.PullOff()
 	}
-
-	fmt.Println ("done with this crazyness")
-	pin.PullOff()
 	
 	fmt.Println ("exiting")
 }
